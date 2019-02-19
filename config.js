83227a32d5a12235c21aa8fda322cf4003c03e40
danishrabbani1100 <43536996+danishrabbani1100@users.noreply.github.com>
2019-02-19 17:34:00 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.config = {
     mysql      : {
-        host            : "104.196.15.116",
+        host            : "35.196.203.193",
         user            : "admin",
         password        : "mysql",
         database        : "vox_cinemas",
